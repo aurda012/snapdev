4b0d2b35d249ca141ffaf40afbbfceeb6ad6ab7a
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-24 21:56:28 -0400

    Added Post Details page and GridPostList

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SignInForm from "./_auth/forms/SignInForm";
 import SignUpForm from "./_auth/forms/SignUpForm";
-import { Home, CreatePost, EditPost } from "./_root/pages";
+import { Home, CreatePost, EditPost, PostDetails } from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "./components/ui/toaster";
@@ -23,6 +23,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
         </Route>
       </Routes>
       <Toaster />
