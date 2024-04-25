0434dbfd6b97e410a75782af708c2b9b4f9c8b8f
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-24 21:39:30 -0400

    Added Edit Post page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./globals.css";
 import SignInForm from "./_auth/forms/SignInForm";
 import SignUpForm from "./_auth/forms/SignUpForm";
-import { Home, CreatePost } from "./_root/pages";
+import { Home, CreatePost, EditPost } from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "./components/ui/toaster";
@@ -22,6 +22,7 @@ const App = () => {
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
         </Route>
       </Routes>
       <Toaster />
--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -4,7 +4,7 @@ export { default as Home } from "./Home";
 export { default as CreatePost } from "./CreatePost";
 // export { default as Profile } from "./Profile";
 // export { default as UpdateProfile } from "./UpdateProfile";
-// export { default as EditPost } from "./EditPost";
+export { default as EditPost } from "./EditPost";
 // export { default as PostDetails } from "./PostDetails";
 // export { default as LikedPosts } from "./LikedPosts";
 // export { default as AllUsers } from "./AllUsers";
