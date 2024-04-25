fd8807362e4e6b97e8de18350b9ac2cb0d3a14fd
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-25 18:39:43 -0400

    Fixed component import

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import TopBar from "@/components/shared/TopBar";
+import TopBar from "@/components/shared/Topbar";
 import BottomBar from "@/components/shared/BottomBar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 
